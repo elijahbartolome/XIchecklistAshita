29a65dc9b1e19b32e85788c89c385c432ade1ee2
HiPotionQ8 <166940326+HiPotionQ8@users.noreply.github.com>
2026-03-15 17:57:38 +0300

    Add files via upload

---
+++ maps/warps_waypoints.lua
@@ -0,0 +1,82 @@
+return {
+    [1]  = 'Western Adoulin',
+    [2]  = 'Western Adoulin',
+    [3]  = 'Western Adoulin',
+    [4]  = 'Western Adoulin',
+    [5]  = 'Western Adoulin',
+    [6]  = 'Western Adoulin',
+    [7]  = 'Western Adoulin',
+    [8]  = 'Western Adoulin',
+    [9]  = 'Western Adoulin',
+
+    [10] = 'Eastern Adoulin',
+    [11] = 'Eastern Adoulin',
+    [12] = 'Eastern Adoulin',
+    [13] = 'Eastern Adoulin',
+    [14] = 'Eastern Adoulin',
+    [15] = 'Eastern Adoulin',
+    [16] = 'Eastern Adoulin',
+    [17] = 'Eastern Adoulin',
+    [18] = 'Eastern Adoulin',
+
+    [19] = 'Yahse Hunting Grounds',
+    [20] = 'Yahse Hunting Grounds',
+    [21] = 'Yahse Hunting Grounds',
+    [22] = 'Yahse Hunting Grounds',
+
+    [23] = 'Ceizak Battlegrounds',
+    [24] = 'Ceizak Battlegrounds',
+    [25] = 'Ceizak Battlegrounds',
+    [26] = 'Ceizak Battlegrounds',
+
+    [27] = 'Foret de Hennetiel',
+    [28] = 'Foret de Hennetiel',
+    [29] = 'Foret de Hennetiel',
+    [30] = 'Foret de Hennetiel',
+    [31] = 'Foret de Hennetiel',
+
+    [32] = 'Morimar Basalt Fields',
+    [33] = 'Morimar Basalt Fields',
+    [34] = 'Morimar Basalt Fields',
+    [35] = 'Morimar Basalt Fields #4',
+    [36] = 'Morimar Basalt Fields',
+    [37] = 'Morimar Basalt Fields',
+
+    [38] = 'Yorcia Weald',
+    [39] = 'Yorcia Weald',
+    [40] = 'Yorcia Weald',
+    [41] = 'Yorcia Weald',
+
+    [42] = 'Marjami Ravine',
+    [43] = 'Marjami Ravine',
+    [44] = 'Marjami Ravine',
+    [45] = 'Marjami Ravine',
+    [46] = 'Marjami Ravine',
+
+    [47] = 'Kamihr Drifts',
+    [48] = 'Kamihr Drifts',
+    [49] = 'Kamihr Drifts',
+    [50] = 'Kamihr Drifts',
+    [51] = 'Kamihr Drifts',
+    
+	--[[
+	[100] = 'Jeuno',
+	[200] = 'Northern San d\'Oria', -- warp runes
+	[201] = 'Bastok Markets', -- warp runes
+	[202] = 'Windurst Woods', -- warp runes
+	[203] = 'Selbina', -- warp runes
+	[204] = 'Mhaura', -- warp runes
+	[205] = 'Kazham', -- warp runes
+	[206] = 'Rabao', -- warp runes
+	[207] = 'Norg', -- warp runes
+
+	[208] = 'Tavnazian Safehold', -- warp runes
+	[209] = 'Aht Urhgan Whitegate', -- warp runes
+	[210] = 'Nashmau', -- warp runes
+
+	[300] = 'Rala Waterways', -- Augural Conveyors
+	[301] = 'Cirdas Caverns', -- Augural Conveyors
+	[302] = 'Yorcia Weald', -- Augural Conveyors
+	[303] = 'Outer Ra\'Kaznar', -- Augural Conveyors
+	]]
+}
